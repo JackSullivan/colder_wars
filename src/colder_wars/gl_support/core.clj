60b2ca223b5604b6d7330e38febfb4a10568a8c0
John Sullivan <john.t.sullivan@gmail.com>
2013-12-03 17:07:45 -0500

    Updates to gl_support/core. We have a triangle.

--- src/colder_wars/gl_support/core.clj
+++ src/colder_wars/gl_support/core.clj
@@ -1,4 +1,5 @@
 (ns colder-wars.gl-support.core
+  (:use (colder-wars.gl-support.OpenGLException))
   (:import
    (java.nio FloatBuffer)
    (org.lwjgl BufferUtils)
@@ -8,32 +9,63 @@
 (defonce varrays (agent {}))
 (defonce vbuffers (agent {}))
 
-(defn- push-array (partial send varrays assoc))
-(defn- push-buffer (partial send vbuffers assoc))
+(def push-array (partial send varrays assoc))
+(def push-buffer (partial send vbuffers assoc))
+
+(defn- clear-vbuffers []
+  (map #(GL15/glDeleteBuffers %) (vals @vbuffers)))
+(defn- clear-varrays []
+  (map #(GL30/glDeleteVertexArrays %) (vals @varrays)))
+
+(defn- check-gl-exception []
+  (let [error-val (GL11/glGetError)]
+    (if (not= GL11/GL_NO_ERROR error-val)
+       (throw (Exception. (str "OpenGL reported error:" (GLU/gluErrorString error-val)))))))
+
+(defn- prepare-display
+  ([title]
+   (prepare-display [800 600] (-> (ContextAttribs. 3 2)
+                                (.withProfileCore true)
+                                (.withForwardCompatible true)) (PixelFormat.) title))
+  ([[width height] ^ContextAttribs context ^PixelFormat pixel-format title]
+   (Display/setDisplayMode (DisplayMode. width height))
+   (Display/create pixel-format context)
+   (Display/setTitle title)
+   (GL11/glViewport 0 0 width height)))
 
 (defmacro report-opengl-errors
   [form]
-  `(let [result ~form
-         error-val (GL11/glGetError)]
-     (if (not= GL11/GL_NO_ERROR error-val)
-       (throw (OpenGLException. (str "OpenGL reported error:" (GLU/gluErrorString error-val)))))
-     result))
+  `(let [result# ~form]
+     (check-gl-exception)
+     result#))
 
-(defmacro with-varray
+(defmacro with-gl-exceptions
+  [& body]
+  `(do ~@(interleave body (repeat `(check-gl-exception)))))
+
+(defmacro with-varray ;; TODO add ability to retrieve stored
   [varr & body]
-  `(let [varr-id (report-opengl-errors (GL30/glGenVertexArrays))]
-     (push-array ~varr varr-id)
-     (report-opengl-errors (GL30/glBindVertexArray varr-id))
+  `(let [varr-id# (report-opengl-errors (GL30/glGenVertexArrays))]
+     (push-array ~varr varr-id#)
+     (report-opengl-errors (GL30/glBindVertexArray varr-id#))
      (do ~@body)
      (report-opengl-errors (GL30/glBindVertexArray 0))))
 
-(defmacro with-vbuffer
+(defmacro with-vbuffer ;; TODO Add ability to retrieve stored
   [vbuf buffer-type & body]
-  `(let [vbuf-id (report-opengl-errors (GL30/glGenBuffers))]
-     (push-buffer ~vbuf vbuf-id)
-     (report-opengl-errors (GL30/glBindBuffers buffer-type vbuf-id))
+  `(let [vbuf-id# (report-opengl-errors (GL15/glGenBuffers))]
+     (push-buffer ~vbuf vbuf-id#)
+     (report-opengl-errors (GL15/glBindBuffer ~buffer-type vbuf-id#))
      (do ~@body)
-     (report-opengl-errors (GL30/glBindBuffers buffer-type 0))))
+     (report-opengl-errors (GL15/glBindBuffer ~buffer-type 0))))
+
+(defmacro with-opengl-context
+  [title & body]
+  `(do (prepare-display ~title)
+     ~@body
+     (clear-vbuffers)
+     (clear-varrays)
+     (Display/destroy))) ;; Other cleanup actions here
 
 (defn ^FloatBuffer float-buffer
   [& flts]
@@ -43,3 +75,25 @@
         (.put arr)
         (.flip))))
 
+(with-opengl-context "VAO High-Level Test"
+  (GL11/glClearColor (float 0.4) (float 0.6) (float 0.9) (float 0))
+  (with-varray :arr
+    (with-vbuffer :buf GL15/GL_ARRAY_BUFFER
+      (GL15/glBufferData GL15/GL_ARRAY_BUFFER
+                     (float-buffer
+                      -0.5 0.5 0.0 ; left bottom triangle
+                      -0.5 -0.5 0.0
+                      0.5 -0.5 0.0
+                      0.5 -0.5 0.0 ; right top triangle
+                      0.5 0.5 0.0
+                      -0.5 0.5 0.0)
+                     GL15/GL_STATIC_DRAW)
+      ))
+  (while (not (Display/isCloseRequested))
+    (GL11/glClear (bit-or GL11/GL_COLOR_BUFFER_BIT GL11/GL_DEPTH_BUFFER_BIT))
+    (Display/update)
+    (Display/sync 60)))
+;;   (with-varray
+;;     (with-vbuffer))))
+
+;; (Display/destroy)
