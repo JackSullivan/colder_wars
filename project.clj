f7918c5384b1d8d93bc74b41c9d0dd1072151cae
John Sullivan <john.t.sullivan@gmail.com>
2013-12-09 18:13:57 -0500

    Added high-concept

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
+                 [so.modernized.high-concept "0.0.1-SNAPSHOT"]
                  [com.badlogicgames.gdx/gdx "0.9.9"]
                  [com.badlogicgames.gdx/gdx-backend-lwjgl "0.9.9"]
                  [com.badlogicgames.gdx/gdx-platform "0.9.9"
