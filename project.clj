70d7fa2ba636c23a9035967d600ba74ae2c22bbb
John Sullivan <john.t.sullivan@gmail.com>
2013-12-11 20:53:24 -0500

    Added warn-on-reflect and updated to use libgdx snapshots.

--- project.clj
+++ project.clj
@@ -3,14 +3,15 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [so.modernized.high-concept "0.0.1-SNAPSHOT"]
-                 [com.badlogicgames.gdx/gdx "0.9.9"]
-                 [com.badlogicgames.gdx/gdx-backend-lwjgl "0.9.9"]
-                 [com.badlogicgames.gdx/gdx-platform "0.9.9"
+                 [com.badlogicgames.gdx/gdx "1.0-SNAPSHOT"]
+                 [com.badlogicgames.gdx/gdx-backend-lwjgl "1.0-SNAPSHOT"]
+                 [com.badlogicgames.gdx/gdx-platform "1.0-SNAPSHOT"
                   :classifier "natives-desktop"]
-                 [com.badlogicgames.gdx/gdx-bullet-platform "0.9.9"
+                 [com.badlogicgames.gdx/gdx-bullet-platform "1.0-SNAPSHOT"
                   :classifier "natives-desktop"]
-                 [com.badlogicgames.gdx/gdx-bullet "0.9.9"]]
+                 [com.badlogicgames.gdx/gdx-bullet "1.0-SNAPSHOT"]]
+  :repositories [["sonatype-snapshots" {:url "http://oss.sonatype.org/content/repositories/snapshots"}]]
+  :global-vars {*warn-on-reflection* true}
   :source-paths ["src/clojure"]
   :java-source-paths ["src/java"]
   :aot [#"so.modernized.colder-wars.*"]
